c5dc36ea7a7c979ebbaa617936aa69e09c76d389
William Gan <ganweiliang@gmail.com>
2018-02-13 09:49:20 +0800

    Added formatAsWelcomeMessage method to Formatter class

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.ui;
 
+import static seedu.addressbook.common.Messages.MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE;
+import static seedu.addressbook.common.Messages.MESSAGE_USING_STORAGE_FILE;
+import static seedu.addressbook.common.Messages.MESSAGE_WELCOME;
+
 public class Formatter {
 
     /** A decorative prefix added to the beginning of lines printed by AddressBook */
@@ -13,8 +17,22 @@ public class Formatter {
     /** Format of indexed list item */
     private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
 
+    /** Format welcome message to be shown to the user */
+    public static String showWelcomeMessage(String version, String storageFilePath) {
+        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
+        return formatMessage(
+                DIVIDER,
+                DIVIDER,
+                MESSAGE_WELCOME,
+                version,
+                MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE,
+                storageFileInfo,
+                DIVIDER);
+    }
+
+
     /** Format message(s) to be shown to the user */
-    public String formatMessage(String... message) {
+    public static String formatMessage(String... message) {
         final StringBuilder formatted = new StringBuilder();
         for (String m : message) {
             formatted.append(LINE_PREFIX).append(m.replace("\n", LS + LINE_PREFIX)).append("\n");
