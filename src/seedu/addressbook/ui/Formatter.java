dc5612d25bdd745f299ee6cc7260aacdf2d7b1c7
William Gan <ganweiliang@gmail.com>
2018-02-13 10:14:14 +0800

    Shifted responsibility of showing of command result to Formatter class

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,5 +1,10 @@
 package seedu.addressbook.ui;
 
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static seedu.addressbook.common.Messages.*;
 
 public class Formatter {
@@ -16,7 +21,7 @@ public class Formatter {
     private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
 
     /** Format welcome message to be shown to the user */
-    public static String formatWelcomeMessage(String version, String storageFilePath) {
+    static String formatWelcomeMessage(String version, String storageFilePath) {
         String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
         return formatMessage(
                 DIVIDER,
@@ -29,22 +34,54 @@ public class Formatter {
     }
 
     /** Format goodbye message to be shown to the user */
-    public static String formatGoodbyeMessage() {
+    static String formatGoodbyeMessage() {
         return formatMessage(MESSAGE_GOODBYE, DIVIDER, DIVIDER);
     }
 
     /** Format initialisation failed message to be shown to the user */
-    public static String formatInitFailedMessage() {
+    static String formatInitFailedMessage() {
         return formatMessage(MESSAGE_INIT_FAILED, DIVIDER, DIVIDER);
     }
 
     /** Format message(s) to be shown to the user */
-    public static String formatMessage(String... message) {
+    static String formatMessage(String... message) {
         final StringBuilder formatted = new StringBuilder();
         for (String m : message) {
             formatted.append(LINE_PREFIX).append(m.replace("\n", LS + LINE_PREFIX)).append("\n");
         }
         return formatted.toString();
+    }
+
+    /**
+     * Format a list of persons as an indexed list.
+     * Private contact details are hidden.
+     */
+    static String formatPersonListView(List<? extends ReadOnlyPerson> persons) {
+        final List<String> formattedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : persons) {
+            formattedPersons.add(person.getAsTextHidePrivate());
+        }
+        return formatAsIndexedList(formattedPersons);
+    }
+
+    /** Formats a list of strings as a viewable indexed list. */
+    private static String formatAsIndexedList(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + TextUi.DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatMessage(formatted.toString());
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
     }
 
 }
--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -32,7 +32,6 @@ public class TextUi {
     /** Format of indexed list item */
     private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
 
-
     /** Offset required to convert between 1-indexing and 0-indexing.  */
     public static final int DISPLAYED_INDEX_OFFSET = 1;
 
@@ -117,46 +116,9 @@ public class TextUi {
     public void showResultToUser(CommandResult result) {
         final Optional<List<? extends ReadOnlyPerson>> resultPersons = result.getRelevantPersons();
         if (resultPersons.isPresent()) {
-            showPersonListView(resultPersons.get());
+            showToUser(Formatter.formatPersonListView(resultPersons.get()));
         }
         showToUser(Formatter.formatMessage(result.feedbackToUser, DIVIDER));
-    }
-
-    /**
-     * Shows a list of persons to the user, formatted as an indexed list.
-     * Private contact details are hidden.
-     */
-    private void showPersonListView(List<? extends ReadOnlyPerson> persons) {
-        final List<String> formattedPersons = new ArrayList<>();
-        for (ReadOnlyPerson person : persons) {
-            formattedPersons.add(person.getAsTextHidePrivate());
-        }
-        showToUserAsIndexedList(formattedPersons);
-    }
-
-    /** Shows a list of strings to the user, formatted as an indexed list. */
-    private void showToUserAsIndexedList(List<String> list) {
-        showToUser(Formatter.formatMessage(getIndexedListForViewing(list)));
-    }
-
-    /** Formats a list of strings as a viewable indexed list. */
-    private static String getIndexedListForViewing(List<String> listItems) {
-        final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
-        for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
-        }
-        return formatted.toString();
-    }
-
-    /**
-     * Formats a string as a viewable indexed list item.
-     *
-     * @param visibleIndex visible index for this listing
-     */
-    private static String getIndexedListItem(int visibleIndex, String listItem) {
-        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
     }
 
 }
