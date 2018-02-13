3a2109a9681b21247619d1eafabfd4b34fe40e81
William Gan <ganweiliang@gmail.com>
2018-02-13 09:47:22 +0800

    Added formatMessage method in Formatter class

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -13,5 +13,13 @@ public class Formatter {
     /** Format of indexed list item */
     private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
 
+    /** Format message(s) to be shown to the user */
+    public String formatMessage(String... message) {
+        final StringBuilder formatted = new StringBuilder();
+        for (String m : message) {
+            formatted.append(LINE_PREFIX).append(m.replace("\n", LS + LINE_PREFIX)).append("\n");
+        }
+        return formatted.toString();
+    }
 
 }
