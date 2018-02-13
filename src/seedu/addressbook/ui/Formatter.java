5abeeef4c070bd00b72a02b7e6243b39b51cdbfd
William Gan <ganweiliang@gmail.com>
2018-02-13 09:43:48 +0800

    Created Formatter class and temporarily duplicated decorations

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.ui;
+
+public class Formatter {
+
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    private static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+
+}
